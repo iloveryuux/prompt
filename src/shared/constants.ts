5dea5dd866abd839bf39adeb09d779fa5773a267
iloveryuux <martiin17jr@gmail.com>
2024-08-05 18:25:28 +0200

    style: biome lint

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,7 +2,9 @@ export const KEY_UP = '\u001b[A'
 export const KEY_DOWN = '\u001b[B'
 export const KEY_ENTER = '\r'
 export const KEY_CTRL_C = '\u0003'
-export const TITLE_ICONS = ['ϟ', '〄', '✡', '✩'];
-export const DEFAULT_TITLE_ICON = TITLE_ICONS[Math.floor(Math.random() * TITLE_ICONS.length)];
-export const SELECTION_ICONS = ['›', '▶', '↳'];
-export const DEFAULT_SELECTION_ICON = SELECTION_ICONS[Math.floor(Math.random() * SELECTION_ICONS.length)];
+export const TITLE_ICONS = ['ϟ', '〄', '✡', '✩']
+export const DEFAULT_TITLE_ICON =
+  TITLE_ICONS[Math.floor(Math.random() * TITLE_ICONS.length)]
+export const SELECTION_ICONS = ['›', '▶', '↳']
+export const DEFAULT_SELECTION_ICON =
+  SELECTION_ICONS[Math.floor(Math.random() * SELECTION_ICONS.length)]
