0e760fdd1c61596cdfecd1fb5c47a73ab412cb71
iloveryuux <martiin17jr@gmail.com>
2024-08-05 05:31:53 +0200

    fix: space input

--- src/input/prompt.ts
+++ src/input/prompt.ts
@@ -5,7 +5,7 @@ import type readline from 'node:readline'
 
 export function promptUser(rl: readline.Interface): Promise<string> {
   return new Promise(resolve => {
-    rl.question(blue(DEFAULT_ICON), answer => {
+    rl.question(blue(`${DEFAULT_ICON} `), answer => {
       resolve(answer.trim())
     })
   })
