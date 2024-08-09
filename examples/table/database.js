7dd6d3230b9379968923a1634d2e762759021bdd
iloveryuux <martiin17jr@gmail.com>
2024-08-09 06:03:34 +0200

    docs(examples): add usages for table
    Co-authored-by: Ex 177673749+extraditado@users.noreply.github.com

---
+++ examples/table/database.js
@@ -0,0 +1,10 @@
+import { table } from '@ryuux/prompt'
+
+table({
+  head: ['ID', 'Name', 'Country'],
+  body: [
+    ['1', 'iloveryuux', 'Moon 🌙'],
+    ['2', 'Ryo', 'Marocco '],
+    ['3', 'Ex', 'United States']
+  ]
+})
